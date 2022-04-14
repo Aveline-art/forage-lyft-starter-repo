33739ee1fe565faf8c1c143b7c787920e4b0bf04
avali4907 <avali4907@outlook.com>
2022-04-14 11:33:22 -0700

    rewrote tests

---
+++ test/test_engine.py
@@ -0,0 +1,55 @@
+import unittest
+
+from engine.model.capulet_engine import CapuletEngine
+from engine.model.sternman_engine import SternmanEngine
+from engine.model.willoughby_engine import WilloughbyEngine
+
+
+class TestCapuletEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 30001
+        last_service_mileage = 0
+
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 30000
+        last_service_mileage = 0
+
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        self.assertFalse(engine.needs_service())
+
+
+class TestSternmanEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        warning_light_is_on = True
+
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        warning_light_is_on = False
+
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(engine.needs_service())
+
+
+class TestWilloughbyEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 60001
+        last_service_mileage = 0
+
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 60000
+        last_service_mileage = 0
+
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        self.assertFalse(engine.needs_service())
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_tire.py
@@ -0,0 +1,36 @@
+import unittest
+
+from tire.model.carrigan import CarriganTire
+from tire.model.octoprime import OctoprimeTire
+
+
+class TestCarriganTire(unittest.TestCase):
+    def test_tire_should_be_serviced(self):
+        sensor_values = [0.9, 0, 0, 0]
+
+        tire = CarriganTire(sensor_values)
+        self.assertTrue(tire.needs_service())
+
+    def test_tire_should_not_be_serviced(self):
+        sensor_values = [0.89, 0.89, 0.89, 0.89]
+
+        tire = CarriganTire(sensor_values)
+        self.assertFalse(tire.needs_service())
+
+
+class TestOctoprimeTire(unittest.TestCase):
+    def test_tire_should_be_serviced(self):
+        sensor_values = [1, 1, 1, 0]
+
+        tire = OctoprimeTire(sensor_values)
+        self.assertTrue(tire.needs_service())
+
+    def test_tire_should_not_be_serviced(self):
+        sensor_values = [1, 1, 0.99, 0]
+
+        tire = OctoprimeTire(sensor_values)
+        self.assertFalse(tire.needs_service())
+
+
+if __name__ == '__main__':
+    unittest.main()
