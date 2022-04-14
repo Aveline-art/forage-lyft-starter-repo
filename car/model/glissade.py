9fddbb36021c9c6d9e8e642ab302a1135aa684ec
Ava Li <62368440+Aveline-art@users.noreply.github.com>
2022-04-14 10:31:52 -0700

    Rewrote unittests to work with new classes (#3)
    * corrected error in unittest
    
    * refactored classes
    
    * corrected for errors

--- car/model/glissade.py
+++ car/model/glissade.py
@@ -6,7 +6,7 @@ from battery.spindler_battery import SpindlerBattery
 
 class Glissade(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__()
         self.engine = WilloughbyEngine(
             last_service_mileage, current_mileage)
         self.battery = SpindlerBattery(last_service_date)
+        super().__init__(self.engine, self.battery)
